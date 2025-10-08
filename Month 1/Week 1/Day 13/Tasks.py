05cc2fc6c79ce15483a56fd3b648181734b8d7d7
Kings Uthana Idogu <kingsuthanaidogu@gmail.com>
2025-10-08 17:27:12 +0100

    day

--- Month 1/Week 1/Day 13/Tasks.py
+++ Month 1/Week 1/Day 13/Tasks.py
@@ -22,3 +22,56 @@ y = iris.target
 # Split 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 print('Train size', X_train.shape[0], 'Test size:', X_test.shape[0])
+
+
+# Pipeline + GridSearch for RandomForest
+pipe_rf = Pipeline([
+    ('scaler', StandardScaler()),
+    ('rf', RandomForestClassifier(random_state=42))
+])
+
+param_grid_rf = {
+    'rf__n_estimators': [50, 100, 200],
+    'rf__max_depth': [None, 5, 10],
+    'rf__min_samples_split': [2, 5]
+}
+
+gs_rf = GridSearchCV(pipe_rf, param_grid_rf, cv=5, scoring='accuracy', n_jobs=-1)
+gs_rf.fit(X_train, y_train)
+
+print('Best RF params:', gs_rf.best_params_)
+print('Best RF CV score:', gs_rf.best_score_)
+
+
+# Evaluate on test set
+best_rf = gs_rf.best_estimator_
+y_pred_rf = best_rf.predict(X_test)
+print('Test Accuracy (RF):', accuracy_score(y_test, y_pred_rf))
+print('n\Classification Report (RF):\n', classification_report(y_test, y_pred_rf, target_names=iris.target_names))
+
+# 3. RandomizedSearchCV - SVC (example for larger search space)
+# Pipeline + RandomSearch for SVC
+pipe_svc = Pipeline([
+    ('scaler', StandardScaler()),
+    ('svc', SVC(random_state=42))
+])
+
+from scipy.stats import expon, randint
+
+param_dist_svc = {
+    'svc__C': expon(scale=1.0), # continious distribution
+    'svc__gamma': expon(scale=0.1),
+    'svc_kernel': ['rbf', 'poly']
+}
+
+rs_svc = RandomizedSearchCV(pipe_svc, param_dist_svc, n_iter=20, cv=5, scoring='accuracy', n_jobs=-1, random_state=42)
+rs_svc .fit(X_train, y_train)
+
+print('Best RF params:', gs_rf.best_params_)
+print('Best RF CV score:', gs_rf.best_score_)
+
+# Evaluate on test set
+best_rf = gs_rf.best_estimator_
+y_pred_rf = best_rf.predict(X_test)
+print('Test Accuracy (RF):', accuracy_score(y_test, y_pred_rf))
+print('\nClassification Report (RF):\n', classification_report(y_test, y_pred_rf, target_names=iris.target_names))
